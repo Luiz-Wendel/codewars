b1c97e3a2e8cfd50c7d86be1747f22a79aa06ef1
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-06 19:23:22 -0300

    chore: add kata dubstep

---
+++ src/6-kyu/dubstep.js
@@ -0,0 +1,7 @@
+/* Source: https://www.codewars.com/kata/551dc350bf4e526099000ae5 */
+
+function songDecoder(song) {
+  return song.replace(/(WUB)+/g, ' ').trim();
+}
+
+module.exports = { songDecoder };
